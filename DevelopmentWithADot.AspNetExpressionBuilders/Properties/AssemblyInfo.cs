f670f7efc915d2ad058a68baa6ffc334ff17e1f4
Ricardo Peres <rjperes@hotmail.com>
2013-06-18 17:09:32 +0100

    Fixed assembly metadata

--- DevelopmentWithADot.AspNetExpressionBuilders/Properties/AssemblyInfo.cs
+++ DevelopmentWithADot.AspNetExpressionBuilders/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DevelopmentWithADot.ExpressionBuilders")]
+[assembly: AssemblyTitle("DevelopmentWithADot.AspNetExpressionBuilders")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DevelopmentWithADot.ExpressionBuilders")]
+[assembly: AssemblyProduct("DevelopmentWithADot.AspNetExpressionBuilders")]
 [assembly: AssemblyCopyright("Copyright © Ricardo Peres 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
